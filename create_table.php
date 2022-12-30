3eb15caf32fb93c9c7b1a45864c4a6298a49fe35
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2022-12-30 22:40:36 +0530

    Update create_table.php

--- create_table.php
+++ create_table.php
@@ -1,35 +1,3 @@
-<?php
-// Connect to the database
-$host = "sql6.freemysqlhosting.net";
-$username = "sql6587280";
-$password = "lL9IzSLKt6";
-$dbname = "sql6587280";
+<?php 
 
-$conn = mysqli_connect($host, $username, $password, $dbname);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-// Get the input from the user
-$productid = $_POST['productid'];
-$productname = $_POST['productname'];
-$productprice = $_POST['productprice'];
-$productexpiry = $_POST['productexpiry'];
-
-// Create the table
-$sql = "CREATE TABLE products (
-   productid INTEGER PRIMARY KEY,
-   productname VARCHAR(255),
-   productprice DECIMAL(10,2),
-   productexpiry DATE
-)";
-
-if (mysqli_query($conn, $sql)) {
-    echo "Table created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-?>
+$conn= new mysqli('sql6.freemysqlhosting.net','sql6587280','lL9IzSLKt6','sql6587280')or die("Could not connect to mysql".mysqli_error($con));
