2067067077b1ee2333f85a417b7016253788ada4
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2022-12-31 01:00:41 +0530

    Update process.php

--- process.php
+++ process.php
@@ -1,22 +1,22 @@
 <?php
-
-
-
-  $server = "sql6.freemysqlhosting.net";
-  $username = "sql6587280";
-  $password = "lL9IzSLKt6";
-
-
-  $conn = mysqli_connect($server, $username, $password);
-  mysqli_select_db($conn, "sql6587280");
-
-  $name = $_POST['name'];
-  $address = $_POST['address'];
-  $phone = $_POST['phone'];
-
-  $sql = "INSERT INTO users (name, address, phone) VALUES ('$name', '$address', '$phone')";
-  mysqli_query($conn, $sql);
-
-
-  mysqli_close($conn);
+$servername = "sql6.freemysqlhosting.net";
+$database = "sql6587280";
+$username = "sql6587280";
+$password = "lL9IzSLKt6";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+ 
+$sql = "INSERT INTO users (name, address, phone) VALUES ('Test', 'Testing', '43433')";
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
 ?>
